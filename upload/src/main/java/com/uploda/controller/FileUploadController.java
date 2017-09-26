2edd86ecc3d43d6472553534be8795e4be82617d
阳君 <937447974@qq.com>
2017-09-26 16:13:10 +0800

    上传项目初始化

---
+++ upload/src/main/java/com/uploda/controller/FileUploadController.java
@@ -0,0 +1,19 @@
+package com.uploda.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * FileUploadController.java
+ * <p>
+ * Created by didi on 2017/9/26.
+ * Copyright © 2017年 upload. All rights reserved.
+ */
+public class FileUploadController {
+
+    @RequestMapping(value = "/fileUpload")
+    public void fileUpload() {
+        // http://www.plupload.com
+
+    }
+
+}
