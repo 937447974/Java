46b1b49f15e086efe0819478fd814e975d2919bb
阳君 <937447974@qq.com>
2017-12-15 17:37:36 +0800

    更改类名

--- java/src/main/java/com/yjcocoa/reflection/ReflectTest.java
+++ java/src/main/java/com/yjcocoa/reflection/ReflectTest.java
@@ -13,47 +13,27 @@ import java.lang.reflect.Method;
  * @author 阳君
  * @version 1.0
  */
-public class ReflectionTest {
+public class ReflectTest {
 
     public static void main(String[] args) throws Exception {
 //        testClass();
 //        testConstructor();
 //        testField();
-//        testMethod();
+        testMethod();
     }
 
     static void testClass() throws Exception {
         Class class1 = User.class;
         System.out.println(class1.getName());
-
-        //第二种：Class c2 = code1.getClass();
+        //第二种
         User demo2 = new User();
         Class c2 = demo2.getClass();
         System.out.println(c2.getName());
-
-        //第三种：Class c3 = Class.forName("com.trigl.reflect.Code");
+        //第三种
         Class class3 = Class.forName("com.yjcocoa.reflection.User");
         System.out.println(class3.getName());
     }
 
-    static void testConstructor() throws Exception {
-        Class uc = User.class;
-        // 获取所有构造器
-        for (Constructor constructor : uc.getDeclaredConstructors()) {
-            System.out.println(constructor);
-        }
-        System.out.println();
-        // 获取所有public 构造器
-        for (Constructor constructor : uc.getConstructors()) {
-            System.out.println(constructor);
-        }
-        System.out.println();
-        // private 构造器 初始化
-        Constructor<User> constructor = uc.getDeclaredConstructor(String.class);
-        constructor.setAccessible(true); // private 需要开启允许访问
-        User user = constructor.newInstance("YJ");
-        System.out.println(user);
-    }
 
     static void testField() throws Exception {
         Class uc = User.class;
@@ -74,6 +54,25 @@ public class ReflectionTest {
         User user = new User();
         field.set(user, "YJ"); // set 方法
         System.out.println(user.getName() + "；" + field.get(user)); // get 方法
+    }
+
+    static void testConstructor() throws Exception {
+        Class uc = User.class;
+        // 获取所有构造器
+        for (Constructor constructor : uc.getDeclaredConstructors()) {
+            System.out.println(constructor);
+        }
+        System.out.println();
+        // 获取所有public 构造器
+        for (Constructor constructor : uc.getConstructors()) {
+            System.out.println(constructor);
+        }
+        System.out.println();
+        // private 构造器 初始化
+        Constructor<User> constructor = uc.getDeclaredConstructor(String.class);
+        constructor.setAccessible(true); // private 需要开启允许访问
+        User user = constructor.newInstance("YJ");
+        System.out.println(user);
     }
 
     static void testMethod() throws Exception {
